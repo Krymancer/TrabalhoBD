214fa0a30951bf88a90f2109636c0317fdd3d686
Salmo Mascarenhas <salmo.cruz@gmail.com>
2019-06-03 23:16:57 -0300

    Update PontoTuristico.cs
    Adicionado atributos e metodos(get set).

--- BDTur/Classes/PontoTuristico.cs
+++ BDTur/Classes/PontoTuristico.cs
@@ -8,5 +8,37 @@ namespace BDTur.Classes
 {
     class PontoTuristico
     {
+        private int idPontoTuristico;
+        private string tipoPontoTuristico;
+        private string endTipoPontoTuristico;
+        private string endLogradouroPontoTuristico;
+        private string endNumeroPontoTuristico;
+        private string endComplementoPontoTuristico;
+        private string endBairroPontoTuristico;
+        private string endCepPontoTuristico;
+        private int cidade_idCidade;
+
+        public PontoTuristico(int idPontoTuristico, string tipoPontoTuristico, string endTipoPontoTuristico, string endLogradouroPontoTuristico, string endNumeroPontoTuristico, string endComplementoPontoTuristico, string endBairroPontoTuristico, string endCepPontoTuristico, int cidade_idCidade)
+        {
+            this.idPontoTuristico = idPontoTuristico;
+            this.tipoPontoTuristico = tipoPontoTuristico;
+            this.endTipoPontoTuristico = endTipoPontoTuristico;
+            this.endLogradouroPontoTuristico = endLogradouroPontoTuristico;
+            this.endNumeroPontoTuristico = endNumeroPontoTuristico;
+            this.endComplementoPontoTuristico = endComplementoPontoTuristico;
+            this.endBairroPontoTuristico = endBairroPontoTuristico;
+            this.endCepPontoTuristico = endCepPontoTuristico;
+            this.cidade_idCidade = cidade_idCidade;
+        }
+
+        public int IdPontoTuristico { get; set; }
+        public string TipoPontoTuristico { get; set; }
+        public string EndTipoPontoTuristico { get; set; }
+        public string EndLogradouroPontoTuristico { get; set; }
+        public string EndNumeroPontoTuristico { get; set; }
+        public string EndComplementoPontoTuristico { get; set; }
+        public string EndBairroPontoTuristico { get; set; }
+        public string EndCepPontoTuristico { get; set; }
+        public int Cidade_idCidade { get; set; }
     }
 }
