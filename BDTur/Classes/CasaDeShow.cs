415d47eaa75569994a3da41079a2d1ee2daad703
Salmo Mascarenhas <salmo.cruz@gmail.com>
2019-06-07 17:13:39 -0300

    *Todas as classes finalizadas
    Criação de todas as classes concluída.
    *Exceto engano.

--- BDTur/Classes/CasaDeShow.cs
+++ BDTur/Classes/CasaDeShow.cs
@@ -8,11 +8,26 @@ namespace BDTur.Classes
 {
     class CasaDeShow : PontoTuristico
     {
+        private int idCasadeShow;
+        private string diaFechamentoCasadeShow;
+        private string horaInicioCasadeShow;
+        private int pontoTuristicoIdPontoTuristico;
+        private int restauranteIdRestaurante;
 
-        public CasaDeShow(int idPontoTuristico, string tipoPontoTuristico, string nomePontoTuristico, string contatoPontoTuristico, string descricaoPontoTuristico, string endTipoPontoTuristico, string endLogradouroPontoTuristico, string endNumeroPontoTuristico, string endComplementoPontoTuristico, string endBairroPontoTuristico, string endCepPontoTuristico, int cidadeIdCidade)
+        public CasaDeShow(int idCasadeShow, string diaFechamentoCasadeShow, string horaInicioCasadeShow, int pontoTuristicoIdPontoTuristico, int restauranteIdRestaurante, int idPontoTuristico, string tipoPontoTuristico, string nomePontoTuristico, string contatoPontoTuristico, string descricaoPontoTuristico, string endTipoPontoTuristico, string endLogradouroPontoTuristico, string endNumeroPontoTuristico, string endComplementoPontoTuristico, string endBairroPontoTuristico, string endCepPontoTuristico, int cidadeIdCidade)
             : base(idPontoTuristico, tipoPontoTuristico, nomePontoTuristico, contatoPontoTuristico, descricaoPontoTuristico, endTipoPontoTuristico, endLogradouroPontoTuristico, endNumeroPontoTuristico, endComplementoPontoTuristico, endBairroPontoTuristico, endCepPontoTuristico, cidadeIdCidade)
         {
-
+            this.idCasadeShow = idCasadeShow;
+            this.diaFechamentoCasadeShow = diaFechamentoCasadeShow;
+            this.horaInicioCasadeShow = horaInicioCasadeShow;
+            this.pontoTuristicoIdPontoTuristico = pontoTuristicoIdPontoTuristico;
+            this.restauranteIdRestaurante = restauranteIdRestaurante;
         }
+
+        public int IdCasadeShow { get; set; }
+        public string DiaFechamentoCasadeShow { get; set; }
+        public string HoraInicioCasadeShow { get; set; }
+        public int PontoTuristicoIdPontoTuristico { get; set; }
+        public int RestauranteIdRestaurante { get; set; }
     }
 }
